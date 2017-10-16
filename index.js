7ca80750e82062a62a918767014550db5823974a
Adam Heath <adam@polleverywhere.com>
2017-10-16 12:14:51 +0800

    Simplify code and escape HTML
     - Move haml-coffee to peerDependency
     - Remove loaderUtils (it's not actually used)
     - Allow haml-coffee to escape HTML
     - Convert thrown strings to Error for webpack

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-var loaderUtils = require("loader-utils");
+const hamlc = require("haml-coffee")
+
 module.exports = function(source) {
-  var hamlc, query, req, template, result;
-  this.cacheable && this.cacheable(true);
-  // 
-  // Requires
-  // 
-  hamlc     = require("haml-coffee");
-  // 
-  // Loader
-  // 
-  query     = loaderUtils.parseQuery(this.query);
-  req       = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
-  // 
-  // Compilation
-  // 
-  template  = hamlc.template(source, null, null, {placement: 'standalone', escapeHtml: false});
-  result    = "module.exports =" + template.toString();
-  return result;
+  if(this.cacheable) {
+    this.cacheable(true);
+  }
+  try {
+    let template  = hamlc.template(source, null, null, {
+      placement: 'standalone',
+      escapeHtml: true
+    });
+    return "module.exports =" + template.toString();
+  } catch(e) {
+    if(!(e instanceof Error)) e = new Error(e);
+    throw e;
+  }
 }
